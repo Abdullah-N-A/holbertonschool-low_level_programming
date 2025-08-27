d7cc4cce923a2145d925cd32a13501d9f9c7aeeb
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-08-27 11:24:14 +0000

    new task

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+\**
+*main print the size of various types
+*
+*return alwayes 0 sucsess
+*
+**/
+
+int main(void)
+{
+printf("size of  a char:\n "sizeof(char));
+printf("size of an int:\n  "sizeof(int));
+printf("size of a long int\n"sizeof(long int));
+printf("size of a long long int \n"sizeof(long long int));
+printf("size of a float \n"sizeof(float));
+
+return(0)
+}
