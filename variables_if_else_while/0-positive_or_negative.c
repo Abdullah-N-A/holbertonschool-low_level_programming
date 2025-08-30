b622de2d9eb75ddf93b3144f7a1adff6b70db7dd
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-08-30 15:14:05 +0000

    new up

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,8 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));/* Use the current time as the seed for the random number */
-	n = rand() - RAND_MAX / 2;  /* Generate a random number between -RAND_MAX/2 and RAND_MAX/2 */
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
