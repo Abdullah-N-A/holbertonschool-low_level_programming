2de511c690c6934b6e0c0345851d57cbd57c36a5
root <root@ip-10-42-67-196.ec2.internal>
2025-09-01 12:18:19 +0000

    new task

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+write a func that print numbers from 0 to 9
+
+
+
+
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
