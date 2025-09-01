28acf91fdba6dd0ae08fed8f463efabb2b7ec6ea
root <root@ip-10-42-67-196.ec2.internal>
2025-09-01 11:01:02 +0000

    new up

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-*checks for digets (0 through 9)
-*
-*
-*retun 1 if c is a digit,0 other
-*/
+ * _isdigit - checks for digits (0 through 9)
+ * @c: the character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
