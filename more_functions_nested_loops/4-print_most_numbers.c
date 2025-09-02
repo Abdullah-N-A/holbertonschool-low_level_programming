42a30689f78d07b247e70ebd26f3fb6553574ccc
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-02 13:10:00 +0000

    new task

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+write a function that prints numbers fromm 0 yo 9
+
+exepet 2 and 4 
+
+
+
+*/
+void print_most_numbers(void)
+{
+int i;
+for (i=0; i<=9; i++)
+{
+	if (i==2 || i==4){
+	continue;}
+	_putchar(i + '0');
+}
+ 	_putchar('\n');
+
+
+
+}
