219d6b9016b0baded984108e51c8ffde687298c7
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-02 13:42:29 +0000

    new tasl

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Return: void
+ */
+
+
+void more_numbers(void){
+    int i, j;
+    char c[] = "01234567891011121314";
+
+    for (i = 0; i <= 9; i++){
+        for (j = 0; c[j] != '\0'; j++){
+            _putchar(c[j]);
+        }
+        _putchar('\n');
+    }
+}
