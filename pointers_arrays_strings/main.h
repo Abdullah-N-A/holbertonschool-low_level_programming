03e925b82ff875c9b7a73cbe4badc883a6048e57
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-06 19:03:44 +0000

    new up

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,4 +12,30 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
+/* 0-strcat.c */
+char *_strcat(char *dest, char *src);
+
+/* 1-strncat.c */
+char *_strncat(char *dest, char *src, int n);
+
+/* 2-strncpy.c */
+char *_strncpy(char *dest, char *src, int n);
+
+/* 3-strcmp.c */
+int _strcmp(char *s1, char *s2);
+
+/* 4-rev_array.c */
+void reverse_array(int *a, int n);
+
+/* 5-string_toupper.c */
+char *string_toupper(char *str);
+
+/* 6-cap_string.c */
+char *cap_string(char *str);
+
+/* 7-leet.c */
+char *leet(char *str);
+
+/* 8-rot13.c */
+char *rot13(char *str);
 #endif /* MAIN_H */
