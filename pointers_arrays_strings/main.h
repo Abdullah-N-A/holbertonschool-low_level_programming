df30c8d521a92d4836595657674ff6e665e4dc93
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-13 14:27:01 +0000

    new up

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #define MAIN_H
+void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
 int _pow_recursion(int x, int y);
