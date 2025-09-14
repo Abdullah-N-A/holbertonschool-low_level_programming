c1610c206352882abdd3a888c71772570cc1dbc7
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-14 15:44:10 +0000

    new up

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -15,24 +15,22 @@ char *_strdup(char *str)
     char *dup;
     unsigned int i, length = 0;
 
-    /* if str is NULL, return NULL immediately */
     if (str == NULL)
         return (NULL);
 
-    /* calculate length of the string */
     while (str[length] != '\0')
         length++;
 
-    /* allocate memory for copy (+1 for null terminator) */
+
     dup = malloc(sizeof(char) * (length + 1));
     if (dup == NULL)
         return (NULL);
 
-    /* copy characters one by one */
+
     for (i = 0; i < length; i++)
         dup[i] = str[i];
 
-    /* add null terminator at the end */
+
     dup[length] = '\0';
 
     return (dup);
