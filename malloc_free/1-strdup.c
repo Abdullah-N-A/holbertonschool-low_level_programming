fbdee72a7524b4faf2a7cc6647a2c8954d9af64f
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-14 15:47:47 +0000

    new up

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,27 +12,23 @@
  */
 char *_strdup(char *str)
 {
-    char *dup;
-    unsigned int i, length = 0;
+	char *dup;
+	unsigned int i, length = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    while (str[length] != '\0')
-        length++;
+	while (str[length] != '\0')
+		length++;
 
+	dup = malloc(sizeof(char) * (length + 1));
+	if (dup == NULL)
+		return (NULL);
 
-    dup = malloc(sizeof(char) * (length + 1));
-    if (dup == NULL)
-        return (NULL);
+	for (i = 0; i < length; i++)
+		dup[i] = str[i];
 
+	dup[length] = '\0';
 
-    for (i = 0; i < length; i++)
-        dup[i] = str[i];
-
-
-    dup[length] = '\0';
-
-    return (dup);
+	return (dup);
 }
-
