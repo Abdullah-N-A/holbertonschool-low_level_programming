307cb3049df01991e94af37860823eb6da6336fc
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-15 12:20:16 +0000

    new up

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -1,40 +1,40 @@
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * argstostr - Concatenates all the arguments of a program.
- * @ac: Number of arguments.
- * @av: Array of argument strings.
+ * argstostr - Concatenates all arguments of a program
+ * @ac: Number of arguments
+ * @av: Array of argument strings
  *
- * Return: Pointer to a new string containing all arguments separated
- *         by '\n', or NULL if ac == 0, av == NULL, or memory allocation fails.
+ * Return: Pointer to a new string, with each argument
+ *         followed by '\n', or NULL if ac == 0, av == NULL,
+ *         or memory allocation fails
  */
 char *argstostr(int ac, char **av)
 {
-    int i, len = 0, pos = 0;
-    char *result;
+    int i, j, total_len = 0, pos = 0;
+    char *str;
 
     if (ac == 0 || av == NULL)
         return (NULL);
 
-    /* Calculate total length required including '\n' for each argument */
-    for (i = 0; i < ac; i++)
-        len += strlen(av[i]) + 1; /* +1 for '\n' */
+        for (i = 0; i < ac; i++)
+        total_len += strlen(av[i]) + 1; 
 
-    /* Allocate memory for the concatenated string (+1 for '\0') */
-    result = malloc(sizeof(char) * (len + 1));
-    if (result == NULL)
+    str = malloc(sizeof(char) * (total_len + 1));
+    if (str == NULL)
         return (NULL);
 
-    /* Copy each argument followed by '\n' */
+
     for (i = 0; i < ac; i++)
     {
-        strcpy(result + pos, av[i]);
-        pos += strlen(av[i]);
-        result[pos++] = '\n';
+        for (j = 0; av[i][j]; j++)
+            str[pos++] = av[i][j];
+        str[pos++] = '\n';
     }
 
-    result[pos] = '\0'; /* Null-terminate the string */
+    str[pos] = '\0';
 
-    return (result);
+    return (str);
 }
