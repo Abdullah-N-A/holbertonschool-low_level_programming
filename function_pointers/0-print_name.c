947f8594b955146d27a8ed156eb1db7983132005
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-22 14:13:00 +0000

    new up

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using the function pointer provided
+ * print_name - prints a name using the given function pointer
  * @name: name of the person
  * @f: pointer to a function that takes a char * and returns void
+ *
+ * Description: This function calls the function f on the given name.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-        f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
